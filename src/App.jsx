2f70b2e4f185dbf644da579b3a547fb9591b3bbb
Oyisbe <onyu8407@naver.com>
2024-05-30 15:30:18 +0900

    feat: detail 및 장르/지역별 동적 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./styles/App.css";
 
 import { Route, Routes } from "react-router-dom";
 
-import Main from "./pages/Landing";
+// 로그인 사이드
+import Main from "./pages/Main";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Forgot from "./pages/Forgot";
@@ -12,17 +13,29 @@ import Signed from "./pages/Signed";
 import PwReset from "./pages/PwReset";
 import AfterPwReset from "./pages/AfterPwReset";
 
+// 메인사이드
+import ConcertDetail from "./pages/ConcertDetail";
+import MainGenre from "./pages/MainGenre";
+import MainLocation from "./pages/MainLocation";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* 메인 -> 동적 라우팅 필요  */}
+        {/* navBar를 통해 이동 : 정적 라우팅 */}
         <Route path="/" element={<Main />} />
+
+        {/* 동적 라우팅 설정 : 각 공연에 맞는 상세 페이지  */}
+        <Route path="/concertDetail/:concertID" element={<ConcertDetail />} />
+
+        {/* 동적 라우팅 : 지역별 / 장르별에 맞는 버튼 클릭시 분기  */}
+        <Route path="/genre/:genre" element={<MainGenre />} />
+        <Route path="/location/:location" element={<MainLocation />} />
 
         {/* 로그인 관련 사이드  */}
         <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
-        <Route path="/signed" element={<Signed />} /> 
+        <Route path="/signed" element={<Signed />} />
 
         {/* 계정 정보 찾기 */}
         <Route path="/forgot" element={<Forgot />} />
@@ -30,8 +43,6 @@ function App() {
         <Route path="/idFound" element={<IdFound />} />
         <Route path="/pwReset" element={<PwReset />} />
         <Route path="/afterPwReset" element={<AfterPwReset />} />
-        
-        
       </Routes>
     </div>
   );
