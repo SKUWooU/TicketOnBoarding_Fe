c5337c5fb35fb22fb58d83a118a7a52187333a59
Oyisbe <onyu8407@naver.com>
2024-06-19 12:55:54 +0900

    feat: 마이페이지 메뉴 추가

--- src/pages/ReservedList.jsx
+++ src/pages/ReservedList.jsx
@@ -6,52 +6,42 @@ import { useLocation, useNavigate } from "react-router-dom";
 
 import { FaGear } from "react-icons/fa6";
 import { IoTicketOutline } from "react-icons/io5";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../components/AuthContext";
+import axios from "axios";
 
-function MyPage() {
+function ReservedList() {
   const navigate = useNavigate();
   const location = useLocation();
 
   const { loginInfo } = useContext(AuthContext);
 
-  function gotoList() {
-    navigate("/reservedList");
-  }
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/mypage/reservationlist")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        alert("Axios 통신에 실패하였습니다.\n" + err);
+      });
+  }, []);
 
   return (
     <div>
-      <LoginHeader page="마이페이지" />
+      <LoginHeader page="예매한 티켓 확인" />
       <div className={style.innerContainer}>
         <img src={LogoFont} alt="로고 폰트" />
-        <p className={style.notice}>누구에게나 쉬운 원클릭 예매 - 티켓온보딩</p>
         <p className={style.emphasize}>
           <span className={style.apiResult}>
             {loginInfo.nickName} <IoTicketOutline />
           </span>{" "}
           님 안녕하세요 !
         </p>
-        <div className={style.innerMenu}>
-          <p onClick={gotoList}>
-            <FaGear /> 예매한 티켓 확인하기
-          </p>
-          <p>
-            <FaGear /> 비밀번호 변경하기
-          </p>
-          <p>
-            {/* <span className={style.delete}> */}
-            <FaGear /> 회원 탈퇴하기
-            {/* </span> */}
-          </p>
-          {loginInfo.code === 3 ? (
-            <p>
-              <FaGear /> 관리자 페이지로 이동하기
-            </p>
-          ) : null}
-        </div>
+        <p className={style.notice}>현재 보유한 티켓은 n 장 입니다.</p>
       </div>
     </div>
   );
 }
 
-export default MyPage;
+export default ReservedList;
