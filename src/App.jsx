57a2b56ff9d01eeb25cfcd5ac78cc96e63c4a3f6
Oyisbe <onyu8407@naver.com>
2024-06-19 13:08:33 +0900

    feat: 추가된 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Signed from "./pages/Signed";
 import PwReset from "./pages/PwReset";
 import AfterPwReset from "./pages/AfterPwReset";
 
-//소셜 사이드
+// 소셜 사이드
 import NaverCallBack from "./pages/NaverCallBack";
 import GoogleCallBack from "./pages/GoogleCallBack";
 
@@ -23,14 +23,19 @@ import ConcertDetail from "./pages/ConcertDetail";
 import MainGenre from "./pages/MainGenre";
 import MainLocation from "./pages/MainLocation";
 import SearchResult from "./pages/SearchResult";
+
+import Mypage from "./pages/Mypage";
+
+// 예매 관련
 import ConcertReservation from "./pages/ConcertReservation";
 import ReservSuccess from "./pages/ReservSuccess";
-import Mypage from "./pages/Mypage";
+import ReservFail from "./pages/ReservFail";
 
 //DatePicker 관련
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Payment from "./pages/Payment";
+import ReservedList from "./pages/ReservedList";
 
 function App() {
   return (
@@ -40,6 +45,7 @@ function App() {
           {/* navBar를 통해 이동 : 정적 라우팅 */}
           <Route path="/" element={<Main />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/search" element={<SearchResult />} />
 
           {/* 동적 라우팅 설정 : 각 공연에 맞는 상세 페이지  */}
           <Route path="/concertDetail/:concertID" element={<ConcertDetail />} />
@@ -48,10 +54,11 @@ function App() {
             element={<ConcertReservation />}
           />
           <Route path="/genre/:genre" element={<MainGenre />} />
-
+          <Route path="/search" element={<SearchResult />} />
           {/*결제 관련 페이지*/}
           <Route path="/payment" element={<Payment />} />
           <Route path="/reservSuccess" element={<ReservSuccess />} />
+          <Route path="/reservFail" element={<ReservFail />} />
 
           {/* 동적 라우팅 : 지역별 / 장르별에 맞는 버튼 클릭시 분기  */}
           <Route path="/genre/:genre" element={<MainGenre />} />
@@ -73,7 +80,8 @@ function App() {
           <Route path="/pwReset" element={<PwReset />} />
           <Route path="/afterPwReset" element={<AfterPwReset />} />
 
-          <Route path="/search" element={<SearchResult />} />
+          {/* 마이페이지 관련 */}
+          <Route path="/reservedList" element={<ReservedList />} />
         </Routes>
         <LocalizationProvider dateAdapter={AdapterDayjs} />
       </div>
