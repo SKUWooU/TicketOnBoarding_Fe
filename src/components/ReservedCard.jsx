a148944b1b8b16222fc7f0f16b3caa9ada03e238
Oyisbe <onyu8407@naver.com>
2024-06-24 02:04:57 +0900

    feat: 예약 조회 관련 카드 컴포넌트 수정
    concert.status 를 따라 단일 컴포넌트가 모두 사용될 수 있도록 로직 설계
    환불 버튼 디자인 추가 (module.sccs)

---
+++ src/components/ReservedCard.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "../styles/cardList.module.scss";
+import axiosBackend from "../AxiosConfig";
+
+function ReservedCard({
+  concertID,
+  posterUrl,
+  concertName,
+  concertDate,
+  concertTime,
+  createdAt,
+  seatNumber,
+  reservationId,
+  status,
+}) {
+  const navigate = useNavigate();
+  const [refundRequested, setRefundRequested] = useState(false);
+
+  const gotoDetail = () => {
+    navigate(`/concertDetail/${concertID}`);
+  };
+
+  function formatDate(input) {
+    if (!input) return "";
+    const [date, time] = input.split("T");
+    const [year, month, day] = date.split("-");
+    const [hour, minute] = time.split(":");
+    return `${year}년 ${parseInt(month)}월 ${parseInt(day)}일 ${parseInt(hour)}시 ${parseInt(minute)}분`;
+  }
+
+  function formatTime(input) {
+    if (!input) return "";
+    return input.substring(0, 5);
+  }
+
+  function handleRefund(reservationId, event) {
+    event.stopPropagation();
+    axiosBackend
+      .post(`/mypage/cancel/reservation/${reservationId}`, null, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log(response);
+        alert("환불 신청이 완료되었습니다.");
+        setRefundRequested(true);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("환불 신청 중 오류가 발생했습니다.");
+      });
+  }
+
+  return (
+    <div className={style.cardContainer} onClick={() => gotoDetail(concertID)}>
+      <img className={style.img} src={posterUrl} alt="Concert List" />
+      <div>
+        <p className={style.concertName}>{concertName}</p>
+        <p className={style.period}>공연 날짜 : {concertDate}</p>
+      </div>
+      <div>
+        <p className={style.listDetail}>{formatTime(concertTime)}</p>
+        <p className={`${style.listDetail} ${style.emphasize}`}>
+          예약한 좌석 : {seatNumber}
+        </p>
+        <p className={style.listDetail}>결제일 : {formatDate(createdAt)}</p>
+        {status === "결제완료" && !refundRequested && (
+          <button
+            className={style.refundButton}
+            onClick={(event) => handleRefund(reservationId, event)}
+          >
+            환불 신청하기
+          </button>
+        )}
+        {status === "취소신청" && (
+          <button className={`${style.refundButton} ${style.disabled}`}>
+            환불 대기중
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ReservedCard;
